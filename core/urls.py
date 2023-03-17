60767984c086e8000886ad1e414dc6f49076d89c
Yuriy Rybak <jura.ryback007@gmail.com>
2023-03-17 12:29:03 +0200

    Create a users application and extend the User model

--- core/urls.py
+++ core/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from users.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', index_view, name='index'),  # homepage
+    path('users/', include('users.urls'), name='users'),
 ]
 
 if settings.DEBUG:

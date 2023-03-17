770683baa3b41277833e655c3894addaae678538
Yuriy Rybak <jura.ryback007@gmail.com>
2023-03-17 17:43:50 +0200

    Add db_table for users.User

--- users/models.py
+++ users/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
+
+    class Meta:
+        db_table = 'users'

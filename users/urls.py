c85b37f844875132dd2a6e4349f7e44744b26fa2
Yuriy Rybak <jura.ryback007@gmail.com>
2023-03-17 19:05:29 +0200

    Add a login logic for users

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'users'
 
-urlpatterns = []
+urlpatterns = [
+    path('login/', views.user_login_view, name='login'),
+    path('profile/', views.user_profile_view, name='profile'),
+]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from .forms import UserLoginForm
 
 
 def index_view(request):
     return render(request=request, template_name='users/index.html')
+
+
+def user_profile_view(request):
+    return render(request=request, template_name='users/profile.html')
+
+
+def user_login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request=request, user=user)
+                return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserLoginForm()
+    context = {
+        'form': form,
+    }
+    return render(request=request, template_name='users/login.html', context=context)
